e54f56d4b5b305b8578c2019986a2714fb09b362
LaBren90 <101529095+LaBren90@users.noreply.github.com>
2022-07-11 13:55:18 +0100

    imported bootstrap npm

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import Weather from "./Weather.js";
 export default function App() {
   return (
     <div className="App">
-      <Weather
-        city="London"
-        day="Wednesday"
-        date={16}
-        month="March"
-        time="09:00"
-        temperature={20}
-        condition="clear"
-        windspeed={1}
-        humidity={80}
-      />
-      <footer>
-        <a
-          href="https://github.com/LaBren90/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open Source Code
-        </a>{" "}
-        by Laura Brennan
-      </footer>
+      <div className="container">
+        <Weather
+          city="London"
+          day="Wednesday"
+          date={16}
+          month="March"
+          time="09:00"
+          temperature={20}
+          condition="clear"
+          windspeed={1}
+          humidity={80}
+        />
+        <footer>
+          <a
+            href="https://github.com/LaBren90/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open Source Code
+          </a>{" "}
+          by Laura Brennan
+        </footer>
+      </div>
     </div>
   );
 }
