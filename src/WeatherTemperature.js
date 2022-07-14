b7e50dbb044afc11643cab0acbe1a0d793fab7a5
LaBren90 <101529095+LaBren90@users.noreply.github.com>
2022-07-14 15:57:24 +0100

    unit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="today-temp" id="temp-celsius">
-        {Math.round(props.celsius)}
-      </span>
-      <span> °C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("farhenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="today-temp" id="temp-celsius">
+          {Math.round(props.celsius)}
+        </span>
+        <span> °C | </span>
+        <span>
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="WeatherTemperature">
+        <span className="today-temp" id="temp-celsius">
+          {Math.round(fahrenheit)}
+        </span>
+        <span>
+          <a href="/" onClick={showCelsius}>
+            {" "}
+            °C{" "}
+          </a>
+          |{" "}
+        </span>
+        <span>°F</span>
+      </div>
+    );
+  }
 }
