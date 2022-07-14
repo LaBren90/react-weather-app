b6f1acf31b0c15be1630dd940c4f672bf4b5d7d3
LaBren90 <101529095+LaBren90@users.noreply.github.com>
2022-07-14 15:07:47 +0100

    updated iconurl from api response

--- src/Weather.js
+++ src/Weather.js
@@ -6,6 +6,7 @@ import CurrentWeather from "./CurrentWeather";
 
 export default function Weather(props) {
   const [weather, setWeather] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function showWeather(response) {
     setWeather({
@@ -15,22 +16,38 @@ export default function Weather(props) {
       city: response.data.name,
       humidity: response.data.main.humidity,
       condition: response.data.weather[0].description,
-      iconUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
       day: "Wednseday",
       month: "July",
     });
   }
 
+  function search() {
+    const apiKey = "f1c8b46054b3f1ca6ba205c92bdc54e1";
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weather.ready) {
     return (
       <div className="Weather">
-        <form id="search-form">
+        <form id="search-form" onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Search for a city"
             autoFocus
             id="current-city"
+            onChange={handleCityChange}
           />
           <input type="button" className="search-button" value="Search" />
           <input
@@ -95,10 +112,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "f1c8b46054b3f1ca6ba205c92bdc54e1";
-
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showWeather);
+    search();
     return "Loading...";
   }
 }
